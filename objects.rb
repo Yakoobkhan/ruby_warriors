5825491228f411f18c15c66257fd707572bb076e
developer <saifulla.soudagar@gmail.com>
2015-05-10 02:05:02 +0530

    my first commit

--- objects.rb
+++ objects.rb
@@ -2,6 +2,24 @@
 #Communicating to object 
 #Creating chain object
 
-puts self
-puts 2.odd?
-puts 1.next.next
+puts "lesson1"
+puts self 			#checks self  objects
+puts 2.odd?			#checks if its odd
+puts 1.next.next	 # give the next two number 
+
+puts "lesson2"
+puts 1.methods.first    			#returns all methods for all numric value 1
+puts 1.methods.sort		#sorted an array based on element
+puts ['rock','paper','scissor'].index('paper')  #calling an org on objects
+puts 2.between?(1,4)
+
+puts "lesson3"
+  # there are diff ways of calling a single  statement or methods which does an operation 
+  # we  have to chose wisely and rem ruby is a developer friendly language 
+
+  puts 1+2      #best 
+  puts 1.+(2)   #rocket sciense 
+  puts 1.send(:+,2)
+words=["saif","ismail","yakub"]
+puts words [-1]
+  puts words.[](-1)
---
+++ strings.rb
@@ -0,0 +1,7 @@
+#What is String =>Strings are key to communicating with a user and programming.
+#Literal forms =>he interpreter treats anything surrounded with single quotes (') or double quotes(") as a string. In other words, both 'RubyMonk' and "RubyMonk" will create instances of strings. 
+#String Length => return the length of the string 'RubyMonk'. 
+
+puts"Lesson 4"
+puts"RubyMonk"
+puts "RubyMonk".length
